9668fe84f90c43203e23c199807ff556359c54a8
MICAELA SAVIO <saviomicaela@yahoo.com.ar>
2023-04-04 23:14:50 -0300

    botones agregados

---
+++ app.js
@@ -0,0 +1,4 @@
+// alerta del formulario
+let usuario= document.getElementById("usuario").value;
+// el formulario se envia
+alert ("Datos enviados exitosamente. A la brevedad te estaremos respondiendo al mail ingresado")
