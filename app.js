35b52571017a195cd936f2b9622cdcce183a18e4
shazelly <128921619+shazelly@users.noreply.github.com>
2023-04-07 20:27:23 -0300

    cambios en formulario

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 // alerta del formulario
 /*let usuario= document.getElementById("usuario").value;/*
 // el formulario se envia
-alert ("Datos enviados exitosamente. A la brevedad te estaremos respondiendo al mail ingresado")
+alert ("Datos enviados exitosamente. A la brevedad te estaremos respondiendo al mail ingresado")*/
