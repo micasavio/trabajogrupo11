127c2b91dfd25ecc355b3fa4a71cd85ae639e0ce
MICAELA SAVIO <saviomicaela@yahoo.com.ar>
2023-04-11 11:25:09 -0300

    footer2

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 /*let usuario= document.getElementById("usuario").value;/*
 // el formulario se envia
 alert ("Datos enviados exitosamente. A la brevedad te estaremos respondiendo al mail ingresado")*/
+
+
