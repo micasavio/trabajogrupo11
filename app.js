6e5b810a2e60ee66742f843a7d16250f814c59a7
MICAELA SAVIO <saviomicaela@yahoo.com.ar>
2023-04-13 09:10:16 -0300

    cambios en el footer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // alerta del formulario
-/*let usuario= document.getElementById("usuario").value;/*
-// el formulario se envia
-alert ("Datos enviados exitosamente. A la brevedad te estaremos respondiendo al mail ingresado")*/
+let usuario= document.getElementById("usuario").value;
+// el formulario se envia*/
+alert ("Datos enviados exitosamente. A la brevedad te estaremos respondiendo al mail ingresado")
 
-
+ //setTimeout(function (){
+    //alert+("Si compras el curso dentro de los proximos 5 minutos, te regalamos ademas una clase especial sobre cupcakes")},3000)
