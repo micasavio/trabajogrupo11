2b95472e5c2c52ac050833468991ec5727567642
MICAELA SAVIO <saviomicaela@yahoo.com.ar>
2023-04-14 23:18:30 -0300

    ubicar formulario

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 // alerta del formulario
-let usuario= document.getElementById("usuario").value;
+//let usuario= document.getElementById("usuario").value;
 // el formulario se envia*/
-alert ("Datos enviados exitosamente. A la brevedad te estaremos respondiendo al mail ingresado")
+//alert ("Datos enviados exitosamente. A la brevedad te estaremos respondiendo al mail ingresado")
 
  //setTimeout(function (){
     //alert+("Si compras el curso dentro de los proximos 5 minutos, te regalamos ademas una clase especial sobre cupcakes")},3000)
+
+    function hizoClick() {
+      var email = document.getElementById("email23").value;
+      var mensaje = document.getElementById("mensaje23").value;
+      if (nombre == "" || correo == "") {
+          alert("Debes compeltar ambos campos"); 
+      } else {
+      alert("Enviado");
+      }
+    }
